703c0feef398522850e033dea5737975249502f8
WouterLab <panov135@mail.ru>
2022-09-07 19:33:51 +0300

    product items

--- components/ProductItem/ProductItem.jsx
+++ components/ProductItem/ProductItem.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import Chair261 from '../Products/images/image_261.png';
+import Chair268 from '../Products/images/image_268.png';
+import Chair269 from '../Products/images/image_269.png';
+import Chair270 from '../Products/images/image_270.png';
+import Chair265 from '../Products/images/image_265.png';
 
-const ProductItem = () => {
+const ProductItem = ({ src, title, price }) => {
   return (
-    <div>ProductItem</div>
-  )
-}
+    <div className='gradient h-4/5 w-1/3 flex flex-col justify-center items-center'>
+      <Image
+        src={
+          src === 'Chair261'
+            ? Chair261.src
+            : src === 'Chair268'
+            ? Chair268.src
+            : src === 'Chair269'
+            ? Chair269.src
+            : src === 'Chair265'
+            ? Chair265.src
+            : Chair270.src
+        }
+        alt='chairIMG'
+        width={300}
+        height={300}
+      />
+      <div className='flex flex-row justify-around w-full mt-8'>
+        <p>{title}</p>
+        <p>{price}</p>
+      </div>
+    </div>
+  );
+};
 
-export default ProductItem
+export default ProductItem;
--- components/Products/Products.jsx
+++ components/Products/Products.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import ProductItem from '../ProductItem/ProductItem'
+import React from 'react';
+import ProductItem from '../ProductItem/ProductItem';
 
 const Products = () => {
   return (
-    <div className='bg-[#3f4346] h-screen border-[#88929a] border-l-2 border-r-2 flex items-center'>
-        <div className='h-2/3 border-[#88929a] border-t-2 border-b-2 w-full'>
-            <ProductItem />
-        </div>
+    <div className='bg-[#3f4346] h-screen border-[#88929a] border-l-2 border-r-2 flex flex-col justify-center'>
+      <div className='flex justify-start items-start py-6 pl-10'>
+        <h2 className='text-8xl text-white font-semibold w-4/5'>OUR FEATURED PRODUCTS</h2>
+      </div>
+      <div className='h-2/3 border-[#88929a] border-t-2 border-b-2 w-full flex justify-around items-center'>
+        <ProductItem src='Chair261' title='BEIGE WOODEN CHAIR' price={1299} />
+        <ProductItem src='Chair270' title='GREEN CUSH ARMCHAIR' price={1799} />
+      </div>
+      <div className='flex justify-center items-center align-middle py-8'>
+        <h2 className='text-8xl text-white font-semibold w-4/5 text-center'>LOOKBOOK</h2>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
